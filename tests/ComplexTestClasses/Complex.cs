8fbf8daa55a58a33aeed960cacbbb0bac6e414af
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2020-02-28 13:43:12 +0900

    [update]

--- tests/ComplexTestClasses/Complex.cs
+++ tests/ComplexTestClasses/Complex.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using IrrelevantTestClasses;
 using MessagePack;
 
 namespace ComplexTestClasses
 {
+    [MessagePackObject]
+    public sealed class ComplexType
+    {
+        [Key(0)] public IrrelevantType NameArray { get; set; }
+    }
 }
--- tests/IrrelevantTestClasses/Irrelevant.cs
+++ tests/IrrelevantTestClasses/Irrelevant.cs
@@ -1,6 +1,10 @@
-﻿using System;
-using MessagePack;
+﻿using MessagePack;
 
 namespace IrrelevantTestClasses
 {
+    [MessagePackObject]
+    public class IrrelevantType
+    {
+        [Key(0)] public string[] Names { get; } = new string[10];
+    }
 }
