031b6d93a8ed2d4c62ad995f4571f68f795f1345
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2020-03-01 16:28:33 +0900

    [update]Importをより詳細に記述

--- src/Core/Generator/ModuleImporter.cs
+++ src/Core/Generator/ModuleImporter.cs
@@ -14,9 +14,45 @@ namespace MSPack.Processor.Core
             this.module = module;
         }
 
+        private GenericInstanceType Import(GenericInstanceType type)
+        {
+            var answer = new GenericInstanceType(Import(type.ElementType));
+            foreach (var argument in type.GenericArguments)
+            {
+                answer.GenericArguments.Add(Import(argument));
+            }
+
+            return answer;
+        }
+
+        private ByReferenceType Import(ByReferenceType type) => new ByReferenceType(Import(type.ElementType));
+
+        private ArrayType Import(ArrayType type) => new ArrayType(Import(type.ElementType), type.Rank);
+
+        private PointerType Import(PointerType type) => new PointerType(Import(type.ElementType));
+
+        private TypeReference Import(GenericParameter type) => module.ImportReference(type);
+
         public TypeReference Import(TypeReference reference)
         {
-            return ReferenceEquals(module, reference.Module) ? reference : module.ImportReference(reference);
+            if (ReferenceEquals(module, reference.Module))
+                return reference;
+
+            switch (reference)
+            {
+                case ByReferenceType byReference:
+                    return Import(byReference);
+                case ArrayType arrayType:
+                    return Import(arrayType);
+                case GenericInstanceType genericInstanceType:
+                    return Import(genericInstanceType);
+                case PointerType pointerType:
+                    return Import(pointerType);
+                case GenericParameter genericParameter:
+                    return Import(genericParameter);
+                default:
+                    return module.ImportReference(reference);
+            }
         }
 
         public FieldReference Import(FieldReference reference)
@@ -24,9 +60,30 @@ namespace MSPack.Processor.Core
             return ReferenceEquals(module, reference.Module) ? reference : module.ImportReference(reference);
         }
 
+        private GenericInstanceMethod Import(GenericInstanceMethod method)
+        {
+            var answer = new GenericInstanceMethod(Import(method.ElementMethod));
+            foreach (var argument in method.GenericArguments)
+            {
+                answer.GenericArguments.Add(Import(argument));
+            }
+
+            return answer;
+        }
+
         public MethodReference Import(MethodReference reference)
         {
-            return ReferenceEquals(module, reference.Module) ? reference : module.ImportReference(reference);
+            if (ReferenceEquals(module, reference.Module))
+            {
+                return reference;
+            }
+
+            if (reference is GenericInstanceMethod genericInstanceMethod)
+            {
+                return Import(genericInstanceMethod);
+            }
+
+            return module.ImportReference(reference);
         }
     }
 }
