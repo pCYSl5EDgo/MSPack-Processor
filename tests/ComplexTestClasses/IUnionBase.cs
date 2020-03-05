b747c6e10ef758c3c77235db990b14d12d3fed38
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2020-03-05 19:51:13 +0900

    [add-test]

---
+++ tests/ComplexTestClasses/IUnionBase.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) pCYSl5EDgo. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using MessagePack;
+
+namespace ComplexTestClasses
+{
+    [Union(0, typeof(ImplementorStruct))]
+    public interface IUnionBase
+    {
+        int Value { get; }
+    }
+}
---
+++ tests/ComplexTestClasses/ImplementorStruct.cs
@@ -0,0 +1,12 @@
+﻿namespace ComplexTestClasses
+{
+    public struct ImplementorStruct : IUnionBase
+    {
+        public ImplementorStruct(int a)
+        {
+            Value = a;
+        }
+
+        public int Value { get; }
+    }
+}
