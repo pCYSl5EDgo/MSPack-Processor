245654855d163687c6139bebdb9f2ca4e7d9e2e7
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2020-03-09 14:19:00 +0900

    [update]常に実行されないテストを削除

--- tests/Core.Test/EmptyTest.cs
+++ tests/Core.Test/EmptyTest.cs
@@ -4,7 +4,6 @@
 using MessagePack;
 using MessagePack.Resolvers;
 using NUnit.Framework;
-using SimpleTestClasses;
 
 namespace Core.Test
 {
@@ -25,29 +24,5 @@ namespace Core.Test
 
             MessagePackSerializer.DefaultOptions = option;
         }
-
-        private void Test<T>()
-            where T : class, new()
-        {
-            var value = new T();
-            var bytes = MessagePackSerializer.Serialize(value);
-            var other = MessagePackSerializer.Deserialize<T>(bytes);
-            Assert.False(other is null);
-        }
-
-        [Test]
-        public void StructTest()
-        {
-            var value = new EmptyStruct();
-            var bytes = MessagePackSerializer.Serialize(value);
-            var other = MessagePackSerializer.Deserialize<EmptyStruct>(bytes);
-            Assert.True(value.Equals(other));
-        }
-
-        [Test]
-        public void ClassTest() => Test<EmptyClass>();
-
-        [Test]
-        public void SealedClassTest() => Test<EmptySealedClass>();
     }
 }
