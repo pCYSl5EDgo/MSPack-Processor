67c703d74b52d1e54ac06de56a42e7b1a5b4c5a2
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2020-03-09 23:49:46 +0900

    [fix]範囲についてきちんとソートする loadInstructionsについてはtupleのValue - smallestByteをindexにしてはならぬ

--- src/Core/Generator/EmbeddingHelper/Automata/ForSpanLength1Helper.cs
+++ src/Core/Generator/EmbeddingHelper/Automata/ForSpanLength1Helper.cs
@@ -23,6 +23,7 @@ namespace MSPack.Processor.Core.Embed
                     return (embed2, Array.Empty<Instruction>());
             }
 
+            Array.Sort(tuples, tuplesOffset, tuplesCount, sorter);
             var failInstruction = InstructionUtility.LdcI4(-1);
             var smallestByte = (byte)sorter.GetValue(tuples[tuplesOffset]);
             var biggestByte = (byte)sorter.GetValue(tuples[tuplesOffset + tuplesCount - 1]);
@@ -38,8 +39,8 @@ namespace MSPack.Processor.Core.Embed
             {
                 ref readonly var tuple = ref tuples[index];
                 var i = (byte)sorter.GetValue(tuple) - smallestByte;
-                loadingInstructions[i * 2] = switchTable[i] = InstructionUtility.LdcI4(tuple.Index);
-                loadingInstructions[i * 2 + 1] = Instruction.Create(OpCodes.Ret);
+                loadingInstructions[(index - tuplesOffset) * 2] = switchTable[i] = InstructionUtility.LdcI4(tuple.Index);
+                loadingInstructions[(index - tuplesOffset) * 2 + 1] = Instruction.Create(OpCodes.Ret);
             }
 
             return (new[]
