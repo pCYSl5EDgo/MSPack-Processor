5646751ff827da477fcfd7fc3c9ef60bb6648fa9
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2020-03-10 11:18:47 +0900

    [update]ジェネリクスの型引数に非プリミティブ構造体を指定すると死ぬ 型引数を分解してResolveしてValueTypeかどうか調べる Resolveに失敗したらそっとしておこう ValueTypeならそういう風に書き換える

--- src/UnityEditorExtension/Assets/Plugins/MSPack.Processor/Core/CodeAnalysis/Definitions/GenericInstanceVariationFinder.cs
+++ src/UnityEditorExtension/Assets/Plugins/MSPack.Processor/Core/CodeAnalysis/Definitions/GenericInstanceVariationFinder.cs
@@ -34,10 +34,113 @@ namespace MSPack.Processor.Core.Definitions
                     throw new MessagePackGeneratorResolveFailedException("Generic instance variation must be ");
                 }
 
+                RefillValueTypeInfo(attribute, variation);
+
                 list.Add(variation);
             }
 
             return list;
+        }
+
+        private static void RefillValueTypeInfo<TCustomAttribute, TVariation>(TCustomAttribute attribute, TVariation variation)
+            where TCustomAttribute : ICustomAttribute
+            where TVariation : IGenericInstance
+        {
+            foreach (var property in attribute.Properties)
+            {
+                if (property.Name != "NotPrimitiveValueTypeIndices")
+                {
+                    continue;
+                }
+
+                MakeValueType(variation, property);
+            }
+
+            foreach (var argument in variation.GenericArguments)
+            {
+                MakeValueType(argument);
+            }
+        }
+
+        private static void MakeValueType<T>(T variation, CustomAttributeNamedArgument property)
+            where T : IGenericInstance
+        {
+            var valueTypeIndices = (CustomAttributeArgument[])property.Argument.Value;
+            foreach (var valueTypeIndex in valueTypeIndices)
+            {
+                var argument = variation.GenericArguments[(int)(uint)valueTypeIndex.Value];
+                argument.IsValueType = true;
+            }
+        }
+
+        private static void MakeValueType(TypeReference argument)
+        {
+            while (true)
+            {
+                if (argument is ArrayType arrayType)
+                {
+                    argument = arrayType.ElementType;
+                    continue;
+                }
+
+                if (argument is PinnedType pinnedType)
+                {
+                    argument = pinnedType.ElementType;
+                    continue;
+                }
+
+                if (argument is ByReferenceType byReferenceType)
+                {
+                    argument = byReferenceType.ElementType;
+                    continue;
+                }
+
+                if (argument is OptionalModifierType optionalModifierType)
+                {
+                    MakeValueType(optionalModifierType.ElementType);
+                    argument = optionalModifierType.ModifierType;
+                    continue;
+                }
+
+                if (argument is GenericInstanceType genericInstanceType)
+                {
+                    foreach (var genericArgument in genericInstanceType.GenericArguments)
+                    {
+                        MakeValueType(genericArgument);
+                    }
+
+                    MakeValueType(genericInstanceType.ElementType);
+                    if (genericInstanceType.ElementType.IsValueType)
+                    {
+                        argument.IsValueType = true;
+                    }
+                }
+                else if (argument is PointerType pointerType)
+                {
+                    argument.IsValueType = true;
+                    var elementType = pointerType.ElementType;
+                    if (!elementType.IsValueType)
+                    {
+                        elementType.IsValueType = true;
+                    }
+                }
+                else if (!argument.IsValueType && !argument.IsGenericParameter)
+                {
+                    try
+                    {
+                        var resolved = argument.Resolve();
+                        if (resolved.IsValueType)
+                        {
+                            argument.IsValueType = true;
+                        }
+                    }
+                    catch
+                    {
+                        // ignore
+                    }
+                }
+                break;
+            }
         }
     }
 }
